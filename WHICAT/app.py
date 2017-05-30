24aa9b07535a52203a0cce5446317365bf403e74
chrrrrrris <chris.p.chan94@gmail.com>
2017-05-30 16:53:56 +1000

    fixing project

--- WHICAT/app.py
+++ WHICAT/app.py
@@ -2,6 +2,8 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , app.py
@@ -14,6 +16,18 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 #app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+class UrlForm(Form):
+    url = StringField('URL:', validators=[validators.required()])
+
+
+def reduceURL(url):
+    if(url[4] == 's'):
+        return url[8:]
+    else:
+        return url[7:]
+
 
 
 def connect_db():
@@ -61,35 +75,94 @@ def index():
     return render_template('index.html')
 
 
-
-@app.route('/checklist')
-def checklist():
-    return render_template('checklist.html')
+@app.route('/checklist', methods=['GET', 'POST'])
+def enterUrl():
+    form = UrlForm()
+    if request.method == 'POST':
+        name = request.form['URL']
+        print name
+        session['name'] = name
+        return redirect(url_for('question1'))
+    return render_template('checklist.html',
+                           form=form)
 
 
 @app.route('/checklist/question/1', methods=['GET', 'POST'])
 def question1():
-    return render_template('checklist-Q1.html')
+    name = session['name']
+    print "q1"
+    print name
+    return render_template('checklist-Q1.html', name=name, title=reduceURL(name))
 
 @app.route('/checklist/question/2', methods=['GET', 'POST'])
 def question2():
-    return render_template('checklist-Q2.html')
+    name = session['name']
+    return render_template('checklist-Q2.html', name=name)
 
 
 @app.route('/checklist/question/3', methods=['GET', 'POST'])
 def question3():
-    return render_template('checklist-Q3.html')
+    name = session['name']
+    return render_template('checklist-Q3.html', name=name)
 
 
 @app.route('/checklist/question/4', methods=['GET', 'POST'])
 def question4():
-    return render_template('checklist-Q4.html')
+    name = session['name']
+    return render_template('checklist-Q4.html', name=name)
+
+
+@app.route('/checklist/question/5', methods=['GET', 'POST'])
+def question5():
+    name = session['name']
+    return render_template('checklist-Q5.html', name=name)
+
+
+@app.route('/checklist/question/6', methods=['GET', 'POST'])
+def question6():
+    name = session['name']
+    return render_template('checklist-Q6.html', name=name)
+
+
+@app.route('/checklist/question/7', methods=['GET', 'POST'])
+def question7():
+    name = session['name']
+    return render_template('checklist-Q7.html', name=name)
+
+
+@app.route('/checklist/question/8', methods=['GET', 'POST'])
+def question8():
+    name = session['name']
+    return render_template('checklist-Q8.html', name=name)
+
+
+@app.route('/checklist/question/9', methods=['GET', 'POST'])
+def question9():
+    name = session['name']
+    return render_template('checklist-Q9.html', name=name)
+
+
+@app.route('/checklist/question/10', methods=['GET', 'POST'])
+def question10():
+    name = session['name']
+    return render_template('checklist-Q10.html', name=name)
+
+
+@app.route('/checklist/question/11', methods=['GET', 'POST'])
+def question11():
+    name = session['name']
+    return render_template('checklist-Q11.html', name=name)
 
 
 @app.route('/checklist/question/12', methods=['GET', 'POST'])
 def question12():
-    return render_template('checklist-Q12.html')
+    name = session['name']
+    return render_template('checklist-Q12.html', name=name)
+
+@app.route('/checklist/end', methods=['GET', 'POST'])
+def end():
+    return render_template('checklist-end.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
